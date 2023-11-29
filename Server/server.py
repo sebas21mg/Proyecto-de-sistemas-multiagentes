f1d42e7b2586dbec813329f2aeab6483a5506460
Sebastian Moncada <sebas.200221@gmail.com>
2023-11-28 20:14:43 -0600

    Reiniciar a solo usar los códigos de Gil

--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,10 @@
 # TC2008B. Sistemas Multiagentes y Gráficas Computacionales
 # Python flask server to interact with Unity. Based on the code provided by Sergio Ruiz.
-# Octavio Navarro. October 2023git
+# Octavio Navarro. October 2023 
 
 from flask import Flask, request, jsonify
-from model import CityModel
-from agent import Car, Traffic_Light, Destination, Obstacle, Road
+from agents.model import RandomModel
+from agents.agent import RandomAgent, ObstacleAgent
 
 # Size of the board:
 number_agents = 10
@@ -13,10 +13,12 @@ height = 28
 randomModel = None
 currentStep = 0
 
+# This application will be used to interact with Unity
 app = Flask("Traffic example")
 
-
-@app.route('/init', methods=['GET', 'POST'])
+# This route will be used to send the parameters of the simulation to the server.
+# The servers expects a POST request with the parameters in a form.
+@app.route('/init', methods=['POST'])
 def initModel():
     global currentStep, randomModel, number_agents, width, height
 
@@ -28,51 +30,49 @@ def initModel():
 
         print(request.form)
         print(number_agents, width, height)
-        randomModel = CityModel(number_agents, width, height)
 
-        return jsonify({"message": "Parameters recieved, model initiated."})
-    elif request.method == 'GET':
-        number_agents = 10
-        width = 30
-        height = 30
-        currentStep = 0
-        randomModel = CityModel(number_agents, width, height)
+        # Create the model using the parameters sent by Unity
+        randomModel = RandomModel(number_agents, width, height)
 
-        return jsonify({"message": "Default parameters recieved, model initiated."})
+        # Return a message to Unity saying that the model was created successfully
+        return jsonify({"message":"Parameters recieved, model initiated."})
 
-
+# This route will be used to get the positions of the agents
 @app.route('/getAgents', methods=['GET'])
 def getAgents():
     global randomModel
 
     if request.method == 'GET':
-        agentPositions = [{"id": str(a.unique_id), "x": x, "y": 1, "z": z}
-                          for a, (x, z) in randomModel.grid.coord_iter()
-                          if isinstance(a, Car)]
+        # Get the positions of the agents and return them to Unity in JSON format.
+        # Note that the positions are sent as a list of dictionaries, where each dictionary has the id and position of an agent.
+        # The y coordinate is set to 1, since the agents are in a 3D world. The z coordinate corresponds to the row (y coordinate) of the grid in mesa.
+        agentPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for a, (x, z) in randomModel.grid.coord_iter() if isinstance(a, RandomAgent)]
 
-        return jsonify({'positions': agentPositions})
+        return jsonify({'positions':agentPositions})
 
-
+# This route will be used to get the positions of the obstacles
 @app.route('/getObstacles', methods=['GET'])
 def getObstacles():
     global randomModel
 
     if request.method == 'GET':
-        carPositions = [{"id": str(a.unique_id), "x": x, "y": 1, "z": z}
-                        for a, (x, z) in randomModel.grid.coord_iter()
-                        if isinstance(a, Obstacle)]
+        # Get the positions of the obstacles and return them to Unity in JSON format.
+        # Same as before, the positions are sent as a list of dictionaries, where each dictionary has the id and position of an obstacle.
+        carPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for a, (x, z) in randomModel.grid.coord_iter() if isinstance(a, ObstacleAgent)]
 
-        return jsonify({'positions': carPositions})
+        return jsonify({'positions':carPositions})
 
-
+# This route will be used to update the model
 @app.route('/update', methods=['GET'])
 def updateModel():
     global currentStep, randomModel
     if request.method == 'GET':
+        # Update the model and return a message to Unity saying that the model was updated successfully
         randomModel.step()
         currentStep += 1
-        return jsonify({'message': f'Model updated to step {currentStep}.', 'currentStep': currentStep})
+        return jsonify({'message':f'Model updated to step {currentStep}.', 'currentStep':currentStep})
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
+    # Run the flask server in port 8585
     app.run(host="localhost", port=8585, debug=True)
--- Simulacion/Assets/Scripts/AgentController.cs
+++ Simulacion/Assets/Scripts/AgentController.cs
@@ -87,7 +87,8 @@ public class AgentController : MonoBehaviour
 
     bool updated = false, started = false;
 
-    public GameObject agentPrefab, obstaclePrefab, floor;
+    public GameObject agentPrefab, obstaclePrefab;
+    // public GameObject agentPrefab, obstaclePrefab, floor;
     public int NAgents, width, height;
     public float timeToUpdate = 5.0f;
     private float timer, dt;
@@ -97,23 +98,26 @@ public class AgentController : MonoBehaviour
         agentsData = new AgentsData();
         obstacleData = new AgentsData();
 
+        // Initializes the dictionaries to store the agents positions.
+        // prevPositions stores the previous positions of the agents, while currPositions stores the current positions.
         prevPositions = new Dictionary<string, Vector3>();
         currPositions = new Dictionary<string, Vector3>();
 
         agents = new Dictionary<string, GameObject>();
 
-        floor.transform.localScale = new Vector3((float)width/10, 1, (float)height/10);
-        floor.transform.localPosition = new Vector3((float)width/2-0.5f, 0, (float)height/2-0.5f);
-        
+        // Sets the floor scale and position.
+        // floor.transform.localScale = new Vector3((float)width / 10, 1, (float)height / 10);
+        // floor.transform.localPosition = new Vector3((float)width / 2 - 0.5f, 0, (float)height / 2 - 0.5f);
+
         timer = timeToUpdate;
 
         // Launches a couroutine to send the configuration to the server.
         StartCoroutine(SendConfiguration());
     }
 
-    private void Update() 
+    private void Update()
     {
-        if(timer < 0)
+        if (timer < 0)
         {
             timer = timeToUpdate;
             updated = false;
@@ -127,7 +131,7 @@ public class AgentController : MonoBehaviour
 
             // Iterates over the agents to update their positions.
             // The positions are interpolated between the previous and current positions.
-            foreach(var agent in currPositions)
+            foreach (var agent in currPositions)
             {
                 Vector3 currentPosition = agent.Value;
                 Vector3 previousPosition = prevPositions[agent.Key];
@@ -136,22 +140,22 @@ public class AgentController : MonoBehaviour
                 Vector3 direction = currentPosition - interpolated;
 
                 agents[agent.Key].transform.localPosition = interpolated;
-                if(direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+                if (direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
             }
 
             // float t = (timer / timeToUpdate);
             // dt = t * t * ( 3f - 2f*t);
         }
     }
- 
+
     IEnumerator UpdateSimulation()
     {
         UnityWebRequest www = UnityWebRequest.Get(serverUrl + updateEndpoint);
         yield return www.SendWebRequest();
- 
+
         if (www.result != UnityWebRequest.Result.Success)
             Debug.Log(www.error);
-        else 
+        else
         {
             StartCoroutine(GetAgentsData());
         }
@@ -186,65 +190,65 @@ public class AgentController : MonoBehaviour
 
             // Once the configuration has been sent, it launches a coroutine to get the agents data.
             StartCoroutine(GetAgentsData());
-            StartCoroutine(GetObstacleData());
+            // StartCoroutine(GetObstacleData());
         }
     }
 
-    IEnumerator GetAgentsData() 
+    IEnumerator GetAgentsData()
     {
         // The GetAgentsData method is used to get the agents data from the server.
 
         UnityWebRequest www = UnityWebRequest.Get(serverUrl + getAgentsEndpoint);
         yield return www.SendWebRequest();
- 
+
         if (www.result != UnityWebRequest.Result.Success)
             Debug.Log(www.error);
-        else 
+        else
         {
             // Once the data has been received, it is stored in the agentsData variable.
             // Then, it iterates over the agentsData.positions list to update the agents positions.
             agentsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
 
-            foreach(AgentData agent in agentsData.positions)
+            foreach (AgentData agent in agentsData.positions)
             {
                 Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
 
-                    if(!started)
-                    {
-                        prevPositions[agent.id] = newAgentPosition;
-                        agents[agent.id] = Instantiate(agentPrefab, newAgentPosition, Quaternion.identity);
-                    }
-                    else
-                    {
-                        Vector3 currentPosition = new Vector3();
-                        if(currPositions.TryGetValue(agent.id, out currentPosition))
-                            prevPositions[agent.id] = currentPosition;
-                        currPositions[agent.id] = newAgentPosition;
-                    }
+                if (!started)
+                {
+                    prevPositions[agent.id] = newAgentPosition;
+                    agents[agent.id] = Instantiate(agentPrefab, newAgentPosition, Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 currentPosition = new Vector3();
+                    if (currPositions.TryGetValue(agent.id, out currentPosition))
+                        prevPositions[agent.id] = currentPosition;
+                    currPositions[agent.id] = newAgentPosition;
+                }
             }
 
             updated = true;
-            if(!started) started = true;
+            if (!started) started = true;
         }
     }
 
-    IEnumerator GetObstacleData() 
-    {
-        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getObstaclesEndpoint);
-        yield return www.SendWebRequest();
- 
-        if (www.result != UnityWebRequest.Result.Success)
-            Debug.Log(www.error);
-        else 
-        {
-            obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+    // IEnumerator GetObstacleData()
+    // {
+    //     UnityWebRequest www = UnityWebRequest.Get(serverUrl + getObstaclesEndpoint);
+    //     yield return www.SendWebRequest();
 
-            Debug.Log(obstacleData.positions);
+    //     if (www.result != UnityWebRequest.Result.Success)
+    //         Debug.Log(www.error);
+    //     else
+    //     {
+    //         obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
 
-            foreach(AgentData obstacle in obstacleData.positions)
-            {
-                Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
-            }
-        }
-    }
+    //         Debug.Log(obstacleData.positions);
+
+    //         foreach (AgentData obstacle in obstacleData.positions)
+    //         {
+    //             Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+    //         }
+    //     }
+    // }
 }
